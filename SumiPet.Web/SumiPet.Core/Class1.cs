b8759a345d9b027b31ed8f603999432c0f6a6ae9
Stephen (Hoang Le) <hoangl@joblogic.com>
2021-02-24 17:36:15 +0700

    Add Database project

---
+++ SumiPet.Web/SumiPet.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace SumiPet.Core
+{
+    public class Class1
+    {
+    }
+}
---
+++ SumiPet.Web/SumiPet.Core/Class2.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SumiPet.Core
+{
+    class Class2
+    {
+    }
+}
