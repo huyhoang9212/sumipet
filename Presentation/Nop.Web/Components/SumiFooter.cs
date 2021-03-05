8195aa238e337c741a4aac1e66d7d576c8db0ecd
Hoang Le <hoangl@joblogic.com>
2021-03-05 22:39:20 +0700

    Add footer

---
+++ Presentation/Nop.Web/Components/SumiFooter.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Nop.Web.Factories;
+using Nop.Web.Framework.Components;
+
+namespace Nop.Web.Components
+{
+    public class SumiFooterViewComponent : NopViewComponent
+    {
+        private readonly ICommonModelFactory _commonModelFactory;
+
+        public SumiFooterViewComponent(ICommonModelFactory commonModelFactory)
+        {
+            _commonModelFactory = commonModelFactory;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var model = _commonModelFactory.PrepareFooterModel();
+            return View(model);
+        }
+    }
+}
---
+++ Presentation/Nop.Web/Components/SumiTopMenu.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Nop.Web.Factories;
+using Nop.Web.Framework.Components;
+
+namespace Nop.Web.Components
+{
+    public class SumiTopMenuViewComponent : NopViewComponent
+    {
+        private readonly ICatalogModelFactory _catalogModelFactory;
+
+        public SumiTopMenuViewComponent(ICatalogModelFactory catalogModelFactory)
+        {
+            _catalogModelFactory = catalogModelFactory;
+        }
+
+        public IViewComponentResult Invoke(int? productThumbPictureSize)
+        {
+            var model = _catalogModelFactory.PrepareTopMenuModel();
+            return View(model);
+        }
+    }
+}
