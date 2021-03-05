fd55e25b634128f8c21caf58b1f0dab172d4e1b6
Hoang Le <hoangl@joblogic.com>
2021-03-05 23:46:33 +0700

    add services

---
+++ Presentation/Nop.Web/Components/SumiSliders.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Nop.Web.Factories;
+using Nop.Web.Framework.Components;
+
+
+namespace Nop.Web.Components
+{
+
+    public class SumiSlidersViewComponent : NopViewComponent
+    {
+        private readonly ICommonModelFactory _commonModelFactory;
+
+        public SumiSlidersViewComponent(ICommonModelFactory commonModelFactory)
+        {
+            _commonModelFactory = commonModelFactory;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var model = _commonModelFactory.PrepareFooterModel();
+            return View(model);
+        }
+    }
+
+}
