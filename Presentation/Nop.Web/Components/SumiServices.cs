d1e66971f9e179ef157b63cc29499b6dd31b4529
Hoang Le <hoangl@joblogic.com>
2021-03-06 23:28:55 +0700

    Add services

---
+++ Presentation/Nop.Web/Components/SumiServices.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Nop.Web.Factories;
+using Nop.Web.Framework.Components;
+
+
+namespace Nop.Web.Components
+{
+
+    public class SumiServicesViewComponent : NopViewComponent
+    {
+        private readonly ICommonModelFactory _commonModelFactory;
+
+        public SumiServicesViewComponent(ICommonModelFactory commonModelFactory)
+        {
+            _commonModelFactory = commonModelFactory;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var model = _commonModelFactory.PrepareFooterModel();
+            return View(model);
+        }
+    }
+
+}
