bf0dfbc36685f1a45835a979279d9e9198451b53
Stephen (Hoang Le) <hoangl@joblogic.com>
2021-03-19 16:38:53 +0700

    Admin

--- Database/SumiPet.sql
+++ Database/SumiPet.sql
@@ -35,3 +35,42 @@ update BlogPost set CreatedOnUtc = '2020-03-08 14:38:44.8733574' where Id = 4
 update BlogPost set CreatedOnUtc = '2019-03-08 14:38:44.8733574' where Id = 5
 update BlogPost set CreatedOnUtc = '2021-03-07 14:38:44.8733574' where Id = 6
 update BlogPost set CreatedOnUtc = '2021-06-08 14:38:44.8733574' where Id = 7
+
+
+
+
+
+
+
+
+
+
+
+
+select top 100 * from PermissionRecord
+
+select top 100 * from CustomerRole
+
+select top 100 * from [dbo].[PermissionRecord_Role_Mapping] m join PermissionRecord p on m.PermissionRecord_Id = p.Id
+where CustomerRole_Id = 1
+
+select top 100 * from [dbo].[PermissionRecord_Role_Mapping]
+where CustomerRole_Id = 2
+
+insert into  [dbo].[PermissionRecord_Role_Mapping](PermissionRecord_Id, CustomerRole_Id) values(1, 2)
+insert into  [dbo].[PermissionRecord_Role_Mapping](PermissionRecord_Id, CustomerRole_Id) values(3, 2)
+insert into  [dbo].[PermissionRecord_Role_Mapping](PermissionRecord_Id, CustomerRole_Id) values(4, 2)
+insert into  [dbo].[PermissionRecord_Role_Mapping](PermissionRecord_Id, CustomerRole_Id) values(30, 2)
+
+insert into  [dbo].[PermissionRecord_Role_Mapping](PermissionRecord_Id, CustomerRole_Id) values(43, 2)
+
+insert into  [dbo].[PermissionRecord_Role_Mapping](PermissionRecord_Id, CustomerRole_Id) values(7, 2)
+
+insert into  [dbo].[PermissionRecord_Role_Mapping](PermissionRecord_Id, CustomerRole_Id) values(22, 2)
+
+insert into  [dbo].[PermissionRecord_Role_Mapping](PermissionRecord_Id, CustomerRole_Id) values(23, 2)
+
+
+select top 100 * from [dbo].[Customer_CustomerRole_Mapping]
+
+
